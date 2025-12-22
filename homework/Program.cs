b0b2bbd929f97ef475ab693fd1ae385fb6aee118
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-12-22 14:42:39 +0330

    Business card

---
+++ homework/Program.cs
@@ -0,0 +1,20 @@
+﻿//Homework
+
+#region session1
+//Business card
+Console.WriteLine("__________________________");
+//user input
+Console.WriteLine("Hi.Please.... ");
+Console.WriteLine("Enter your name :");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your age :");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your gender :");
+string gender = Console.ReadLine();
+//Console.WriteLine($"your name is {name}, your age is {age}, your gender is {gender}");
+Console.WriteLine("__________________________");
+Console.WriteLine(" name :"+name);
+Console.WriteLine(" age :" + age);
+Console.WriteLine(" gender :" + gender);
+Console.WriteLine("__________________________");
+#endregion
