cb036f6db6e2394c5fa246554591fdcddbabec21
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-12-22 15:01:35 +0330

    part-1(golden ship)

--- inclass/Program.cs
+++ inclass/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Class exercises
 #region session1
 //console:writeLine & readLine core:
-Console.WriteLine("Please enter your name:");
+/*Console.WriteLine("Please enter your name:");
 string name = Console.ReadLine();
 Console.WriteLine("Please enter your Last name:");
 string lastName = Console.ReadLine();
@@ -9,7 +9,17 @@ Console.WriteLine("Please enter your age :");
 int age =Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"your name is : {name} , your last name is :{lastName},your age is: {age}");
-Console.WriteLine("your name is :"+""+ name , "your last name is :" + lastName ,"your age is:" +""+ age);
+Console.WriteLine("your name is :"+""+ name , "your last name is :" + lastName ,"your age is:" +""+ age);*/
 #endregion
 #region session2
 //برنامه ایی که از کاربر وزن جعبه را بگیر و یکسری شرط را لحاظ کند:
+//value
+int costPerKilo = 100;
+int totalPrice = 0;
+
+Console.WriteLine("Welcome to the Golden Cargo Ship");
+//user input
+Console.WriteLine("please enter  cargo weight : ");
+int cargoWeight =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"your cargo weight is{cargoWeight}kg.");
+//condition
