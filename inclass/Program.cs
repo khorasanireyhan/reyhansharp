d01bc5989ec7f6b3afdc493126abb50c2ef8b37b
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-12-22 14:13:04 +0330

    session1

---
+++ inclass/Program.cs
@@ -0,0 +1,15 @@
+﻿// Class exercises
+#region session1
+//console:writeLine & readLine core:
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter your Last name:");
+string lastName = Console.ReadLine();
+Console.WriteLine("Please enter your age :");
+int age =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"your name is : {name} , your last name is :{lastName},your age is: {age}");
+Console.WriteLine("your name is :"+""+ name , "your last name is :" + lastName ,"your age is:" +""+ age);
+#endregion
+#region session2
+//برنامه ایی که از کاربر وزن جعبه را بگیر و یکسری شرط را لحاظ کند:
