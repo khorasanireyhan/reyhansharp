ab0921c59c0fbd6bc6cd1b2d58194a4e342e9da8
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-12-22 17:59:11 +0330

    Golden Cargo Ship program

--- inclass/Program.cs
+++ inclass/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Class exercises
-#region session1
+
+/*#region session1
 //console:writeLine & readLine core:
-/*Console.WriteLine("Please enter your name:");
+Console.WriteLine("Please enter your name:");
 string name = Console.ReadLine();
 Console.WriteLine("Please enter your Last name:");
 string lastName = Console.ReadLine();
@@ -9,8 +10,8 @@ Console.WriteLine("Please enter your age :");
 int age =Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"your name is : {name} , your last name is :{lastName},your age is: {age}");
-Console.WriteLine("your name is :"+""+ name , "your last name is :" + lastName ,"your age is:" +""+ age);*/
-#endregion
+Console.WriteLine("your name is :"+""+ name , "your last name is :" + lastName ,"your age is:" +""+ age);
+#endregion*/
 #region session2
 //برنامه ایی که از کاربر وزن جعبه را بگیر و یکسری شرط را لحاظ کند:
 //value
@@ -23,3 +24,16 @@ Console.WriteLine("please enter  cargo weight : ");
 int cargoWeight =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"your cargo weight is{cargoWeight}kg.");
 //condition
+if (cargoWeight<=20)
+{
+    totalPrice = cargoWeight * costPerKilo;
+}
+else if(cargoWeight is>20and<=40)
+{
+    costPerKilo += 5;  //costPerKilo=castPerKilo+5;
+    totalPrice = cargoWeight * costPerKilo;
+}
+//output
+Console.WriteLine("your bill is ready .... ");
+Console.WriteLine($"Total Price:{totalPrice}$.");
+#endregion
