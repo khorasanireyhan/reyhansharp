5d4726ec16f2faca422edfc659214af362c300d7
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-12-22 18:40:46 +0330

    calculator_part(1)

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Homework
 
-#region session1
+/*#region session1
 //Business card
 Console.WriteLine("__________________________");
 //user input
@@ -17,4 +17,43 @@ Console.WriteLine(" name :"+name);
 Console.WriteLine(" age :" + age);
 Console.WriteLine(" gender :" + gender);
 Console.WriteLine("__________________________");
+#endregion*/
+
+#region session2
+//Building a simple calculator
+//value
+double result;
+//user input
+Console.WriteLine("..........................");
+Console.WriteLine("Enter your first number :");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your other number: ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("what do you want to do ?");
+Console.WriteLine("(+),(_),(/),(*)");
+string op = Console.ReadLine();
+
+Console.WriteLine("*The operation is in progress*");
+
+//condidtion
+if (op=="+")
+{
+    result = num1 + num1;
+    Console.WriteLine("result is :"+result);
+}
+else if (op=="_")
+{
+    result = num1 - num2;
+    Console.WriteLine("result is:"+result);
+}
+else
+{
+    
+}
+{
+    
+}
+
+Console.WriteLine("..........................");
+
 #endregion
